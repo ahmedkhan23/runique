5dbcc77a2b5e992ffbdc146355eaeeb7c5b7cc1e
ahmedkhan23 <ahmed1.khan@gmail.com>
2025-01-21 11:17:52 -0500

    - jvm ktor convention plugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -44,5 +44,10 @@ gradlePlugin {
             id = "runique.jvm.library"
             implementationClass = "JvmLibraryConventionPlugin"
         }
+
+        register("jvmKtor") {
+            id = "runique.jvm.ktor"
+            implementationClass = "JvmKtorConventionPlugin"
+        }
     }
 }
