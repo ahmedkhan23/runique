989f459e15b59ba84e341ae29405479b53e3a89a
ahmedkhan23 <ahmed1.khan@gmail.com>
2025-01-21 09:06:28 -0500

    - setting up build types for convention plugin ...

--- build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -1,4 +1,6 @@
 import com.android.build.api.dsl.ApplicationExtension
+import com.runique.convention.ExtensionType
+import com.runique.convention.configureBuildTypes
 import com.runique.convention.configureKotlinAndroid
 import com.runique.convention.libs
 import org.gradle.api.Plugin
@@ -24,7 +26,13 @@ class AndroidApplicationConventionPlugin: Plugin<Project> {
                 }
 
                 configureKotlinAndroid(this)
+
+                configureBuildTypes(
+                    this, ExtensionType.APPLICATION
+                )
             }
+
+
         }
     }
 }
